4522ac258badea061133ec62cdb854c9fb7da48d
Najera <xoy638@dca9047643e7.cof.ds.capitalone.com>
2017-08-21 14:19:19 -0400

    Added listener to start game by getting two cards for the player and dealer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,76 @@
 console.log("JS loaded");
+
+var playerHand = [];
+var dealerHand = [];
+
+let cards = [
+  {
+    face:'ace',
+    value: 11
+  },
+  {
+    face: '2',
+    value: 2
+  },
+  { face: '3',
+    value: 3
+  },
+  { face: '4',
+    value: 4
+  },
+  {
+    face: '5',
+    value: 5
+  },
+  { face: '6',
+    value: 6
+  },
+  {
+    face: '7',
+    value: 7
+  },
+  {
+    face: '8',
+    value: 8
+  },
+  {
+    face: '9',
+    value: 9
+  },
+  {
+    face: '10',
+    value: 10
+  },
+  {
+    face: 'king',
+    value: 10
+  },
+  {
+    face: 'queen',
+    value: 10
+  },
+  {
+    face: 'jack',
+    value: 10
+  }];
+
+function getCards(cards, hand){
+  for(let i=0;i < 2; i++){
+    hand.push(cards.shift());
+  }
+}
+
+$(function() {
+  $('.game').on('click', function(event){
+    console.log('button clicked');
+    event.preventDefault();
+
+    cards.sort(function(){return 0.5-Math.random()});
+
+    getCards(cards, dealerHand);
+    getCards(cards, playerHand);
+
+    console.log('dealer', dealerHand);
+    console.log('player', playerHand);
+  })
+})
